603d58508c5be6c68cfe73a7a50b330132973cfc
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-08-15 01:14:16 +0200

    add history

--- src/pages/Company.js
+++ src/pages/Company.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import companies from '../data/company.json';
+import histories from '../data/history.json';
 
 class Company extends Component {
   state = {
     companies: companies,
+    histories: histories,
   }
 
-  showList = (keyName, i) => {
+  showCompany = (keyName, i) => {
     return (
       <li className="company-list" key={i}>
         <p className="title">{keyName[0]}</p>
@@ -15,27 +17,47 @@ class Company extends Component {
     )
   }
 
+  showHistory = (history,index) => {
+    return (
+      <div className="history-card">
+        <p className="history-year">{history.year}年</p>
+        <p className="history-month">{history.month}月</p>
+        <p className="history-event">{history.event}</p>
+      </div>
+    )
+  }
+
   render() {
-    const { companies } = this.state;
+    const { companies, histories } = this.state;
     console.log('companies', companies)
     return (
       <div className="company">
-        <h1>会社概要</h1>
+        <h2 className="company-title">会社概要</h2>
         <div className="company-container">
         {
           companies.map((company, index) => {
             return (
               <div className="company-card" key={index}>
-                <h2>{company.corporateName[1]}</h2>
+                <h2 className="company-name">{company.corporateName[1]}</h2>
                 <ul>
                   {Object.keys(company).map((keyName, i) => (
-                    this.showList(company[keyName], i)
+                    this.showCompany(company[keyName], i)
                   ))}
                 </ul>
               </div>
             )
           })
         }
+        </div>
+        <h2 className="company-title">沿革</h2>
+        <div className="history-container">
+          {
+            histories.map((history,index) => {
+              return (
+                this.showHistory(history, index)
+              )
+            })
+          }
         </div>
       </div>
     )
