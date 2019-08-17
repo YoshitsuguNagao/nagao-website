2466494f037c824b84a9d0cd75670169c61219a0
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-08-17 17:00:25 +0200

    fix home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import news from '../data/news.json'
 
 class Home extends Component {
   render() {
@@ -8,12 +9,29 @@ class Home extends Component {
          <img src={require('../images/IMG_3128.JPG')} alt=""/>
         </section>
         <section className="homepage-news">
-          <h2>新着一覧</h2>
+          <h2 className="title">新着一覧</h2>
+          <div className="news-container">
+            {
+              news.map((item,index) => {
+                return (
+                  <div className="each-news">
+                    <p className="news-date">{item.date}</p>
+                    <p className="news-content">{item.content}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
         </section>
         <section className="homepage-lineup">
-          <h2>充実のラインアップ</h2>
-          <p>油圧式クレーンやトレーラーは最新式を揃えており、最小１２トン吊りから最大４００トン吊りの能力までの各クレーン車を約２９台取り揃えております。</p>
-          <p><a href="/lineup">保有機種一覧</a></p>
+          <h2 className="title">充実のラインアップ</h2>
+          <div className="lineup-container">
+            <div className="lineup-text">
+              <p>油圧式クレーンやトレーラーは最新式を揃えており、最小１２トン吊りから最大４００トン吊りの能力までの各クレーン車を約２９台取り揃えております。</p>
+              <p className="lineup-link"><a href="/lineup">保有機種一覧</a></p>
+            </div>
+            <img className="lineup-img" src={require('../images/IMG_3128.JPG')} alt=""/>
+          </div>
         </section>
       </div>
     )
