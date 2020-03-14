f628860af07e499abd77e786970aa3f241ec615d
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2020-03-14 17:28:10 +0900

    add new Map component

---
+++ src/components/Map.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export class Map extends Component {
+  render() {
+    const { name, address, tel, fax, url } = this.props.item;
+    console.log(name);
+    return (
+      <>
+        <div className="contact-info">
+          <h3 className="contact-name">{name}</h3>
+          <h4 className="contact-content">{address}</h4>
+          <h4 className="contact-content">TEL: {tel}</h4>
+          <h4 className="contact-content">FAX: {fax}</h4>
+        </div>
+        <iframe src={url} frameborder="0" allowfullscreen></iframe>
+      </>
+    );
+  }
+}
+
+export default Map;
