539fb9c909e45501c172de0c963cdeb40aec02cb
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2020-03-14 16:31:04 +0900

    changed the title

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-
   handleClick = () => {
-    const nav = document.querySelector('.nav-links');
-    const burger = document.querySelector('.burger');
-    const navLinks = document.querySelectorAll('.nav-links li');
-      //Toggle Nav
-      nav.classList.toggle('nav-active');
-      //Animate Links
-      navLinks.forEach((link, index) => {
-        if(link.style.animation) {
-          link.style.animation = '';
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
-        }
-      })
-      //Burger Animation
-      burger.classList.toggle('toggle');
-  }
-
+    const nav = document.querySelector(".nav-links");
+    const burger = document.querySelector(".burger");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 +
+          0.5}s`;
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  };
 
   render() {
     return (
       <nav className="navbar-top">
         <div className="nav-title">
           <Link className="nav-title" to="/">
-            <h1 className="navbar-h1">㈱長尾レッカー</h1>
+            <h1 className="navbar-h1">株式会社長尾レッカー</h1>
           </Link>
           <div className="nav-tel">
             {/* <p className="navbar-text">お気軽にお問い合わせください</p>
@@ -38,22 +37,39 @@ class Navbar extends Component {
         </div>
         <div className="navbar-menu">
           <ul className="nav-links">
-            <li><a href="/">ホーム</a></li>
-            <li><a href="/company">会社概要</a></li>
-            <li><a href="/lineup">保有機種</a></li>
-            <li><a href="/business-guide">事業内容</a></li>
-            <li><a href="/recruit">採用情報</a></li>
-            <li><a href="/contact">お問い合わせ</a></li>
+            <li>
+              <a href="/">ホーム</a>
+            </li>
+            <li>
+              <a href="/company">会社概要</a>
+            </li>
+            <li>
+              <a href="/lineup">保有機種</a>
+            </li>
+            <li>
+              <a href="/business-guide">事業内容</a>
+            </li>
+            <li>
+              <a href="/recruit">採用情報</a>
+            </li>
+            <li>
+              <a href="/contact">お問い合わせ</a>
+            </li>
           </ul>
-          <div className="burger" onClick={() => {this.handleClick()}}>
+          <div
+            className="burger"
+            onClick={() => {
+              this.handleClick();
+            }}
+          >
             <div className="line1"></div>
             <div className="line2"></div>
             <div className="line3"></div>
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
 
-export default Navbar
+export default Navbar;
