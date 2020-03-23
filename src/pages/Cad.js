855293e4a28e08c8c33dea2341fa3728a184d92c
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2020-03-23 23:52:03 +0900

    add cad images

--- src/pages/Cad.js
+++ src/pages/Cad.js
@@ -8,8 +8,13 @@ class Cad extends Component {
         <p>各クレーンのCAD図面を元に、工事現場に合わせた施工計画を提案します。</p>
         <p>重量物等の運搬据付ならびに土木建設現場作業に関わる総合的かつ、現場に合った最適な施工計画を提案します。</p>
         <div className="cad-images">
-          <img src="https://res.cloudinary.com/yoshitsugunagao/image/upload/v1570048656/nagao-website/cad1.png" alt=""/>
-          <img src="https://res.cloudinary.com/yoshitsugunagao/image/upload/v1570048656/nagao-website/cad2.png" alt=""/>
+          <img src="https://res.cloudinary.com/yoshitsugunagao/image/upload/v1570048656/nagao-website/cad/cad1.png" alt=""/>
+          <img src="https://res.cloudinary.com/yoshitsugunagao/image/upload/v1570048656/nagao-website/cad/cad2.png" alt="" />
+          <img src="https://res.cloudinary.com/yoshitsugunagao/image/upload/v1570048656/nagao-website/cad/cad3.png" alt=""/>
+          <img src="https://res.cloudinary.com/yoshitsugunagao/image/upload/v1570048656/nagao-website/cad/cad4.png" alt=""/>
+          <img src="https://res.cloudinary.com/yoshitsugunagao/image/upload/v1570048656/nagao-website/cad/cad5.png" alt="" />
+          <img src="https://res.cloudinary.com/yoshitsugunagao/image/upload/v1570048656/nagao-website/cad/cad6.png" alt="" />
+          <img src="https://res.cloudinary.com/yoshitsugunagao/image/upload/v1570048656/nagao-website/cad/cad7.png" alt="" />
         </div>
       </div>
     )
